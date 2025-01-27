d6694f9d81b69d9d52359ca89b10c0d45aa799d9
kishoresarker <kishoresarker.ks@gmail.com>
2025-01-27 16:11:59 +0600

    implement register page

--- app/register/page.js
+++ app/register/page.js
@@ -1,9 +1,33 @@
+import RegisterForm from "@/components/auth/RegisterForm";
+import SocialLogin from "@/components/auth/SocialLogin";
+import Link from "next/link";
 
-const RegisterPage = () => {
+export const metadata = {
+    title: "Bandhu Paribar | Register page",
+    description: "Register page description",
+};
+
+const RegisterPage = async () => {
 
     return (
-        <div>
-           <h1 className="text-center">register page</h1>
+        <div className=" py-[100px] px-5">
+            <div className="max-w-lg mx-auto shadow-lg border px-6 py-7 rounded overflow-hidden">
+                <h2 className="text-2xl uppercase font-medium mb-1">Create an account</h2>
+                <p className="text-gray-600 mb-6 text-sm">
+                    Register for a new user
+                </p>
+
+                <RegisterForm />
+
+                <div className="mt-6 flex justify-center relative">
+                    <div className="text-gray-600 px-3 bg-white z-10 relative">Sign  Up</div>
+                    <div className="absolute left-0 top-3 w-full border-b-2 border-gray-200"></div>
+                </div>
+
+                <SocialLogin />
+
+                <p className="mt-4 text-center text-gray-600">Don&apos;t have an account? <Link href="/login" className="text-[#39b54a] hover:text-[#006837]">Login Now</Link></p>
+            </div>
         </div>
     );
 };
---
+++ components/auth/RegisterForm.jsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { MdError } from "react-icons/md";
+
+const RegisterForm = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+    const onSubmit = async (data) => {
+        console.log(data);
+
+        reset();
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)} action="#" method="post" autoComplete="off">
+                <div className="space-y-2">
+                    <div>
+                        <label htmlFor="name" className="text-[#006837] text-lg font-bold mb-2 block">Full Name</label>
+                        <input type="text"
+                            {...register("name", { required: true })}
+                            name="name"
+                            className={`block w-full border-2 px-4 py-3 text-[#006837] text-sm rounded placeholder-[#006837] ${errors.name ? "focus:outline-red-500 border-red-500" : "focus:outline-[#006837] border-[#006837]"}`}
+                            placeholder="your name" />
+                        {errors.name && <span className="text-sm mt-1 text-red-500"><MdError className="text-lg inline" /> Name field is required.</span>}
+                    </div>
+                    <div>
+                        <label htmlFor="email" className="text-[#006837] text-lg font-bold mb-2 block">Email</label>
+                        <input type="email"
+                            {...register("email", { required: true })}
+                            name="email"
+                            className={`block w-full border-2 px-4 py-3 text-[#006837] text-sm rounded placeholder-[#006837] ${errors.email ? "focus:outline-red-500 border-red-500" : "focus:outline-[#006837] border-[#006837]"}`}
+                            placeholder="youremail.@domain.com" />
+                        {errors.email && <span className="text-sm mt-1 text-red-500"><MdError className="text-lg inline" /> Email field is required.</span>}
+                    </div>
+
+                    <div>
+                        <label htmlFor="password" className="text-[#006837] text-lg font-bold mb-2 block">Password</label>
+                        <div className="relative">
+                            <input type={showPassword ? "text" : "password"}
+                                {...register("password", { required: true })}
+                                name="password"
+                                className={`block w-full border-2 px-4 py-3 text-[#006837] text-sm rounded placeholder-[#006837] ${errors.password ? "focus:outline-red-500 border-red-500" : "focus:outline-[#006837] border-[#006837]"}`}
+                                placeholder="*******" />
+                            {errors.password?.type === "required" && <p className="text-sm mt-1 text-red-500"><MdError className="text-lg inline" /> Password is required.</p>}
+                            <span className="absolute top-4 right-4 text-[#006837]" onClick={() => setShowPassword(!showPassword)}>
+                                {
+                                    showPassword ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>
+                                }
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-6">
+                    <div className="flex items-center">
+                        <input type="checkbox" name="aggrement" id="aggrement"
+                            className="text-lime-500 focus:ring-0 rounded-sm cursor-pointer" />
+                        <label htmlFor="aggrement" className="text-gray-600 ml-3 cursor-pointer">I have read and agree to the <a
+                            href="#" className="text-[#39b54a]">terms & conditions</a></label>
+                    </div>
+                </div>
+                <div className="mt-4">
+                    <button type="submit"
+                        className="block w-full py-2 text-center text-white bg-[#39b54a] border-2 border-[#39b54a] rounded hover:bg-transparent hover:text-[#39b54a] transition uppercase font-roboto font-medium">Create Account</button>
+                </div>
+            </form>
+        </>
+    );
+};
+
+export default RegisterForm;
